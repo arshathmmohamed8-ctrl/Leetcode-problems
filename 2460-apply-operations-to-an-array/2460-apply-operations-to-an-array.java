077f17750ed19e3820efaa1f6a5ece8095ec20e4
Mohamed arshath M <arshathmmohamed8@gmail.com>
2026-09-03 10:06:12 +0530

    Time: 1 ms (95.7%), Space: 45.2 MB (61.09%) - LeetHub

---
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int sum=0;
+        int k=0;
+        int n=nums.length;
+       for(int i=0;i<n-1;i++){
+        if(nums[i]==nums[i+1]){
+            nums[i]*=2;
+            nums[i+1]=0;
+        }
+      }
+      int zero=0;
+      for(int i=0;i<n;i++){
+        if(nums[i]!=0){
+            nums[zero]=nums[i];
+            zero++;
+        }
+      }
+       while(zero<n){
+        nums[zero]=0;
+        zero++;
+       }
+       return nums;
+    }
+}
